a532e612b9c2555c60aec319bfa3b2ea65354f23
Asep Ardi <ardi.winardi91@gmail.com>
2021-08-16 04:13:43 +0700

    lazy load code splitting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import React, { Suspense } from "react";
 import TopNavigation from "./components/TopNavigation";
 import "styles/styles.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Home from "./Home";
-import Transaction from "./Transaction";
-import Account from "./Account";
-import Carts from "./Carts";
-import DetailProduct from "./DetailProduct";
-import Login from "./Login";
-import CartContextProvider from "./contexts/CartContext";
 import AuthContextProvider from "contexts/AuthContext";
+import CartContextProvider from "contexts/CartContext";
+
+const Home = React.lazy(() => import("./Home"));
+const Transaction = React.lazy(() => import("./Transaction"));
+const Account = React.lazy(() => import("./Account"));
+const Carts = React.lazy(() => import("./Carts"));
+const DetailProduct = React.lazy(() => import("./DetailProduct"));
+const Login = React.lazy(() => import("./Login"));
+
 export default function App() {
   return (
     <Router>
@@ -17,12 +19,14 @@ export default function App() {
         <CartContextProvider>
           <TopNavigation />
           <div className="App">
-            <Route exact path="/" component={Home} />
-            <Route exact path="/product/:slug" component={DetailProduct} />
-            <Route exact path="/transactions" component={Transaction} />
-            <Route exact path="/account" component={Account} />
-            <Route exact path="/carts" component={Carts} />
-            <Route exact path="/login" component={Login} />
+            <Suspense fallback={<>Loading...</>}>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/product/:slug" component={DetailProduct} />
+              <Route exact path="/transactions" component={Transaction} />
+              <Route exact path="/account" component={Account} />
+              <Route exact path="/carts" component={Carts} />
+              <Route exact path="/login" component={Login} />
+            </Suspense>
           </div>
         </CartContextProvider>
       </AuthContextProvider>
